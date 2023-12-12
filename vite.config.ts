ab3a8bafc8faf796648fe055d1a962d8284a1f5f
NeerajaNeelima <023neeraja@gmail.com>
2023-12-12 22:40:09 +0530

    added capacitor js

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory
+    target: 'esnext', // Specify the target for the build (e.g., esnext, es2015)
+    minify: 'terser', // Minify the output using Terser
+  },
 })
